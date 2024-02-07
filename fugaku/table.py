8d43c15f7c1c911486b9c6e333c47e6e8380ad00
Patrick Diehl <me@diehlpk.de>
2024-02-06 20:31:41 -0600

    Add regular data

--- fugaku/table.py
+++ fugaku/table.py
@@ -2,15 +2,31 @@ import csv
 import numpy as np
 
 d_sing = []
+d_reg = []
 
 with open('singularity.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         d_sing.append(float(row[0]))
 
-print(len(d_sing))
-print(min(d_sing))
-print(np.median(d_sing))
-print(max(d_sing))
-print(np.mean(d_sing))
-print(np.std(d_sing))
+with open('regular.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        d_reg.append(float(row[0]))
+
+print("singularity")
+print("Elements:",len(d_sing))
+print("Min",min(d_sing))
+print("Median",np.median(d_sing))
+print("Max",max(d_sing))
+print("Average",np.mean(d_sing))
+print("STD",np.std(d_sing))
+print("------------------")
+print("regular")
+print("Elements:",len(d_reg))
+print("Min",min(d_reg))
+print("Median",np.median(d_reg))
+print("Max",max(d_reg))
+print("Average",np.mean(d_reg))
+print("STD",np.std(d_reg))
+print("------------------")
